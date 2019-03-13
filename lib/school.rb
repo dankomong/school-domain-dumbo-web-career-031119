bd95ebf37077a09c824e93207a54d3893df4ab59
Daniel Ko <dankomong@gmail.com>
2019-03-13 19:47:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
 # code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if @roster == {}
+      @roster[grade] = []
+      @roster[grade].push(name)
+    end
+  end
+end
